7f9e224440cc8a3dfce3787d3904232ed3778ac4
Function <34871211+FunctionDJ@users.noreply.github.com>
2021-04-30 21:46:31 +0200

    fix marquee slow

--- src/graphics/parrotfest/main.ts
+++ src/graphics/parrotfest/main.ts
@@ -1,9 +1,6 @@
 /* global nodecg */
-import Marquee3k from "marquee3000";
 import { SocialEntry } from "../../dashboard/data/SocialEntry";
 import { GraphicDataPage } from "../../shared/OverlayData";
-
-Marquee3k.init();
 
 const mapFontAwesomeIcon = (iconName: string): string => {
   switch (iconName.toLowerCase()) {
@@ -11,8 +8,6 @@ const mapFontAwesomeIcon = (iconName: string): string => {
     default: return iconName.toLowerCase();
   }
 };
-
-const bottomBanner = document.querySelector<HTMLDivElement>("#bottom-banner")!;
 
 const djName = document.querySelector<HTMLSpanElement>("#dj-name")!;
 const logo = document.querySelector<HTMLImageElement>("#dj-logo")!;
@@ -22,22 +17,6 @@ const socialsContainer = document.querySelector<HTMLDivElement>("#socials-contai
 const page = window.page;
 
 const replicant = nodecg.Replicant<GraphicDataPage>(`for-graphic-${page}`);
-
-const lineupReplicant = nodecg.Replicant<string>("lineup");
-
-lineupReplicant.on("change", lineup => {
-  Marquee3k.pause(0);
-
-  bottomBanner.innerHTML = `
-    <div class="marquee3k" data-speed="0.5">
-      <p>
-        ${lineup}
-      </p>
-    </div>
-  `;
-
-  Marquee3k.init();
-});
 
 function mapSocialToElement(socialEntry: SocialEntry): HTMLElement {
   const social = document.createElement("div");
---
+++ src/graphics/parrotfest/react.tsx
@@ -0,0 +1,30 @@
+/* global nodecg */
+import * as React from "react";
+import { render } from "react-dom";
+import Marquee from "react-double-marquee";
+
+const lineupReplicant = nodecg.Replicant<string>("lineup");
+
+const App = () => {
+  const [text, setText] = React.useState("Waiting for nodecg...");
+
+  React.useEffect(() => {
+    lineupReplicant.on("change", lineup => setText(lineup));
+  }, []);
+
+  return (
+    <Marquee
+      direction="left"
+      delay={0}
+      speed={0.02}
+      childMargin={0}
+    >
+      {text}
+    </Marquee>
+  );
+};
+
+render(
+  <App/>,
+  document.getElementById("bottom-banner")
+);
